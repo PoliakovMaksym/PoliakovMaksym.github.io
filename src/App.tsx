aad2bd02977911fd66228938a24605ddc07ff34f
PoliakovMaksym <polmax09@gmail.com>
2024-11-10 12:52:50 +0200

    Test new GitHub Actions yaml

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <p>Test GitHub Actions</p>
+        <p>Test GitHub Actions #2</p>
         <a
           className='App-link'
           href='https://reactjs.org'
