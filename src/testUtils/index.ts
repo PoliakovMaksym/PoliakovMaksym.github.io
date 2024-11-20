c1c6f8a74447dcd3f1b7ae566837c913add3bf28
PoliakovMaksym <polmax09@gmail.com>
2024-11-20 11:56:00 +0200

    Fix unit tests

--- src/testUtils/index.ts
+++ src/testUtils/index.ts
@@ -2,3 +2,6 @@ import userEvent from '@testing-library/user-event';
 
 export { userEvent };
 export * from '@testing-library/react';
+
+// Override render method
+export { render } from './customRenderer';
