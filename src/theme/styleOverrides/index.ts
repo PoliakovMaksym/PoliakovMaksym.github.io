0ce95fb0d10c19e2c6dd812bd5c5ce8985eafb6a
Poliakov Maksym <PoliakovMaksym@users.noreply.github.com>
2024-12-29 16:50:33 +0200

    Improve TextField interaction animation (#28)

--- src/theme/styleOverrides/index.ts
+++ src/theme/styleOverrides/index.ts
@@ -1,7 +1,9 @@
 import { buttonOverride } from './Button';
 import { cssBaselineOverride } from './CssBaseline';
+import { outlinedInputOverride } from './TextField';
 
 export const styleOverrides = Object.assign({
   MuiButton: buttonOverride,
   MuiCssBaseline: cssBaselineOverride,
+  MuiOutlinedInput: outlinedInputOverride,
 });
