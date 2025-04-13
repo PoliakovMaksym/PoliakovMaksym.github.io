5e721cd640186d520b58bab00882f5b9af378c4a
PoliakovMaksym <polmax09@gmail.com>
2025-04-13 11:41:29 +0300

    Mass dependencies update

--- src/modules/RaidBossTimers/pages/Home/HomePage.styled.tsx
+++ src/modules/RaidBossTimers/pages/Home/HomePage.styled.tsx
@@ -1,13 +1,13 @@
-import { Grid2, Grid2Props, styled } from '@mui/material';
+import { Grid, GridProps, styled } from '@mui/material';
 
-export const GridContainer = styled(Grid2)<Grid2Props & { spacing: number }>(
+export const GridContainer = styled(Grid)<GridProps & { spacing: number }>(
   ({ theme, spacing }) => ({
     width: '100%',
     maxWidth: `calc(${theme.custom.components.BossCard.maxWidth * 2}px + ${theme.spacing(spacing)})`,
   }),
 );
 
-export const GridItem = styled(Grid2)({
+export const GridItem = styled(Grid)({
   display: 'flex',
   justifyContent: 'center',
 });
